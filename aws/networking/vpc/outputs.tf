135d49a8e78a654392cd3294996ffe05297a383f
Martin Thorpe <martint@Martins-MacBook-Pro.local>
2025-08-29 10:20:57 +0100

    DEV - Directory structure re-org and add NACLs to outputs, tweak to pre-commit config

---
+++ aws/networking/vpc/outputs.tf
@@ -0,0 +1,29 @@
+# We can't use for_each on output blocks (due to expectation of a single value expression, not multiple values from iteration)
+# so we will build a map / list of the values we want (which are the subnet IDs)
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.vpc.id
+}
+
+# k gives us the map key (az-a, az-b, az-c etc)
+# s.id gives us the actual subnet_id, relevant to the key
+output "default_private_subnet_ids" {
+  description = "Map of private subnet IDs by AZ key"
+  value       = { for k, s in aws_subnet.default-private : k => s.id }
+}
+
+output "default_public_subnet_ids" {
+  description = "Map of public subnet IDs by AZ key"
+  value       = { for k, s in aws_subnet.default-public : k => s.id }
+}
+
+output "default-public-nacl" {
+  description = "Default Public Subnet NACL"
+  value       = aws_network_acl.default-public-nacl.id
+}
+
+output "default-private-nacl" {
+  description = "Default Private Subnet NACL"
+  value       = aws_network_acl.default-private-nacl.id
+}
---
+++ aws/networking/vpc/subnets.tf
@@ -0,0 +1,110 @@
+#--- DEFAULT PUBLIC SUBNET
+resource "aws_subnet" "default-public" {
+  vpc_id   = aws_vpc.vpc.id
+  for_each = var.default_subnets
+
+  cidr_block              = each.value.default_public_subnet_cidr
+  availability_zone       = data.aws_availability_zones.available.names[index(keys(var.default_subnets), each.key)]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = {
+    Name        = "${var.vpc_name}-public-${each.key}"
+    Environment = var.environment
+    Provided_By = "terraform"
+  }
+}
+
+resource "aws_network_acl" "default-public-nacl" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.vpc_name}-default-public-nacl"
+  }
+}
+
+resource "aws_network_acl_association" "default-public-nacl-assoc" {
+  for_each       = aws_subnet.default-public
+  subnet_id      = each.value.id
+  network_acl_id = aws_network_acl.default-public-nacl.id
+}
+
+resource "aws_default_route_table" "public" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+
+  tags = {
+    Name        = "${var.vpc_name}-default-public-routetable"
+    Environment = var.environment
+    Provided_By = "terraform"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_default_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+  depends_on             = [aws_vpc.vpc]
+}
+
+resource "aws_route_table_association" "public_rta" {
+  for_each = var.default_subnets
+
+  subnet_id      = aws_subnet.default-public[each.key].id
+  route_table_id = aws_default_route_table.public.id
+}
+
+
+#--- DEFAULT PRIVATE SUBNET
+resource "aws_subnet" "default-private" {
+  vpc_id   = aws_vpc.vpc.id
+  for_each = var.default_subnets
+
+  cidr_block              = each.value.default_private_subnet_cidr
+  availability_zone       = data.aws_availability_zones.available.names[index(keys(var.default_subnets), each.key)]
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name        = "${var.vpc_name}-private-${each.key}"
+    Environment = var.environment
+    Provided_By = "terraform"
+  }
+}
+
+resource "aws_network_acl" "default-private-nacl" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.vpc_name}-default-private-nacl"
+  }
+}
+
+resource "aws_network_acl_association" "default-private-nacl-assoc" {
+  for_each       = aws_subnet.default-private
+  subnet_id      = each.value.id
+  network_acl_id = aws_network_acl.default-private-nacl.id
+}
+
+resource "aws_route_table" "private" {
+  for_each = var.default_subnets
+
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.vpc_name}-private-rt-${each.key}"
+    Environment = var.environment
+    Provided_By = "terraform"
+  }
+}
+
+resource "aws_route" "private_route" {
+  for_each = var.default_subnets
+
+  route_table_id         = aws_route_table.private[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.natgw[each.key].id
+  depends_on             = [aws_route_table.private]
+}
+
+resource "aws_route_table_association" "private_rta" {
+  for_each = var.default_subnets
+
+  subnet_id      = aws_subnet.default-private[each.key].id
+  route_table_id = aws_route_table.private[each.key].id
+}
